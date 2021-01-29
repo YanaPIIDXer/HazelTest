07505a4cb6556cc91c21431922d406c70ef23ac5
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2021-01-29 16:54:24 +0900

    共通の定数定義を追加 とりあえずハンドシェイクコードを定義

---
+++ HazelCommon/Consts.cs
@@ -0,0 +1,13 @@
+namespace HazelCommon
+{
+    /// <summary>
+    /// 定数定義
+    /// </summary>
+    public static class Consts
+    {
+        /// <summary>
+        /// ハンドシェイクコード
+        /// </summary>
+        public static readonly int HandshakeCode = 0xFF00;
+    }
+}
